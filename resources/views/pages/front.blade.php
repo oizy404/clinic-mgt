4c03d47a85858147f82dfb8979447b6fb9aadbf1
oizy404 <71779024+oizy404@users.noreply.github.com>
2021-07-13 19:36:46 +0800

    clinic-mgt

--- resources/views/pages/front.blade.php
+++ resources/views/pages/front.blade.php
@@ -6,18 +6,16 @@
 
 @section('content')
     <div class="front-container1">
-        <div class="row">
+        <div class="row-md">
             <div class="col-md-2 offset-md-5">
                 <img src="images/acdLogo.png" alt="acd logo" class="rounded-circle" id="acdLogo">
             </div>
         </div>
     </div>
     <div class="front-container2">
-        <div class="row">
+        <div class="row-md">
             <div class="col-md-2 offset-md-5">
-                <center>
-                    <button type="Add" class="btn btn-primary" id="btn-login">Log In</button>
-                </center>
+                <button type="Add" class="btn btn-primary" id="btn-login">Log In</button>
             </div>
         </div>
     </div>
