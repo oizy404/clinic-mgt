09da10d863d8c547ed9ce21b23c2cab3420c3f53
oizy404 <71779024+oizy404@users.noreply.github.com>
2021-10-08 19:53:30 +0800

    Update medical-supplies-inventory.blade.php

--- resources/views/pages/medical-supplies-inventory.blade.php
+++ resources/views/pages/medical-supplies-inventory.blade.php
@@ -13,10 +13,7 @@
         <div class="offset-md-1 supplies-inventory-addbtn">
             <div class="col-md-4">
                 <button class="btn" id="btn-item"><i class="fas fa-plus"></i> Add Item</button><br>
-            </div>
-            <div class="col-md-4 offset-md-3 hlth-dta">
-                <h3>MEDICAL SUPPLIES</h3>
-            </div>   
+            </div>    
         </div>
         <div class="offset-md-1 supplies-inventory">
             <table id="supplies-inventory" class="table table-hover" style="width:100%">
@@ -40,13 +37,9 @@
                         <td>{{$med_supply->med_type->medicine_type}}</td>
                         <td>{{$med_supply->stock}}</td>
                         <td>{{$med_supply->expiry_date}}</td>
-                        <td>
-                            <a href="{{route('edit-medical-record', $med_supply->id, $med_types)}}" id="btn-record"><center><i class="far fa-edit"></i></center></a>
-                        </td>
-                        <td><center><i class="far fa-eye"></i></center></td>
-                        <td>
-                            <a href="{{route('delete', $med_supply->id)}}"><center><i class="fas fa-trash-alt"></i></center></a>
-                        </td>
+                        <td><i class="far fa-edit"></i></td>
+                        <td><i class="far fa-eye"></i></td>
+                        <td><i class="fas fa-trash-alt"></i></td>
                     </tr>
                 @endforeach
                 </tbody>
