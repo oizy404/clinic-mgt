fb0641a48b81a6ffa38bf6dd3963bedb3409338f
oizy404 <71779024+oizy404@users.noreply.github.com>
2021-11-16 22:21:07 +0800

    clinic-mgt

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,13 +11,81 @@ class LoginController extends Controller
     public function login(Request $request){
         $credentials = $request->only('username', 'password');
 
-        if (Auth::attempt($credentials)){
+        $users = DB::table('users')->where('username', $request->username)->get("rank"); // gets data from table users where username match
 
-            return redirect()->route('admin-home');
+        $rank;
+        foreach($users as $user){
+        	$rank = $user->rank;    // get the rank of the username. Either 'admin' , 'doctor' , 'supervisor'
 
         }
+
+        if (Auth::attempt($credentials)){
+
+        	$request->session()->put('rank', $rank);    // put session data named 'rank'. Which value is either 'admin', 'doctor' , 'supervisor'
+
+            return redirect()->route('admin-home');
+        }
+
         return back()->withErrors([
             "Invalid Login"
         ]);
     }
+
+    public function loginDoctor(Request $request){
+    	$credentials = $request->only('username', 'password');
+
+    	$users = DB::table('users')->where('username', $request->username)->get("rank");
+
+    	$rank;
+    	foreach($users as $user){
+    	  	$rank = $user->rank;
+    	}
+    	if (Auth::attempt($credentials)) {
+        	$request->session()->put('rank', $rank);
+        	return redirect()->route('doctor-home');
+		}
+        return back()->withErrors([
+            "Invalid Login"
+		]);
+	}
+	
+	public function loginSupervisor(Request $request){
+		$credentials = $request->only('username', 'password');
+
+		$users = DB::table('users')->where('username', $request->username)->get("rank");
+
+		$rank;
+		foreach($users as $user){
+		    $rank = $user->rank;
+		}
+		if (Auth::attempt($credentials)) {
+		    $request->session()->put('rank', $rank);
+		    return redirect()->route('supervisor-home');
+	}
+
+	    return back()->withErrors([
+	        "Invalid Login"
+
+		]);        
+ 	}
+	public function registerPatient(){
+		
+	}
+	public function loginPatient(){
+		$credentials = $request->only('username', 'password');
+
+    	$users = DB::table('users')->where('username', $request->username)->get("rank");
+
+    	$rank;
+    	foreach($users as $user){
+    	  	$rank = $user->rank;
+    	}
+    	if (Auth::attempt($credentials)) {
+        	$request->session()->put('rank', $rank);
+        	return redirect()->route('doctor-home');
+		}
+        return back()->withErrors([
+            "Invalid Login"
+		]);
+	}
 }
